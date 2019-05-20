63606f8adb62a97ca91d1df8c41be62f256c0cde
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2019-05-21 00:55:09 +0300

    update form sending

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -2,17 +2,22 @@ const Form = {
   init() {
     $("form.material__form").on("submit", function(event){
       event.preventDefault();
+      let data = {
+        'name': $(this).find('[name="name"]').val(),
+        'phone': $(this).find('[name="phone"]').val(),
+        'message': $(this).find('[name="message"]').val(),
+      };
       $.ajax({
         type: "POST",
         url: "send.php",
-        data: $(this).serialize(),
-        cache: false,
-        contentType: false,
-        processData: false,
+        data: data,
+        dataType: 'json',
         success: function (response) {
+          console.log(response);
           alert(response.message);
         },
         error: function (response) {
+          console.log(response);
           alert(response.message);
         }
       });
